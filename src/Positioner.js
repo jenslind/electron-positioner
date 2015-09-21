59a1003dec43a544da8ba7d25e32887a9606b05f
Jens Lind <jens.lind@oddhill.se>
2015-09-21 19:39:45 +0200

    Default to right if the window is bigger than the space left
    Because on Windows the window might get out of bounds and dissappear.

--- src/Positioner.js
+++ src/Positioner.js
@@ -68,6 +68,17 @@ export default class Positioner {
       }
     }
 
+    // Default to right if the window is bigger than the space left.
+    // Because on Windows the window might get out of bounds and dissappear.
+    if (position.substr(0, 4) === 'tray') {
+      if ((positions[position].x + windowSize[0]) > screenSize.width) {
+        return {
+          x: positions['topRight'].x,
+          y: positions[position].y
+        }
+      }
+    }
+
     return positions[position]
   }
 
